d86d919839db61ee123cca93acd8c4770f1004de
Samu <samu.linden@gmail.com>
2020-07-22 15:42:24 +0200

    Adds PostgreSQL connection

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import * as Express from "express";
-import { buildSchema, Resolver, Query } from "type-graphql";
-
-@Resolver()
-class HelloResolver {
-  @Query(() => String)
-  async hello() {
-    return "Hello world!";
-  }
-}
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { RegisterResolver } from "./modules/user/Register";
 
 const main = async () => {
+  await createConnection();
+
   const schema = await buildSchema({
     resolvers: [HelloResolver],
   });
