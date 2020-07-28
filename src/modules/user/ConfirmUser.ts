869eb7e82d1f5d40837bafba28e5a1cc25a03481
Samu Linden <samu.linden@gmail.com>
2020-07-28 11:04:31 +0200

    Adds user confirmation emails

---
+++ src/modules/user/ConfirmUser.ts
@@ -0,0 +1,23 @@
+import { Resolver, Mutation, Arg } from "type-graphql";
+
+import { User } from "../../entity/User";
+import { redis } from "../../redis";
+
+@Resolver()
+export class ConfirmUserResolver {
+  @Mutation(() => Boolean)
+  async confirmUser(
+    @Arg("token") token: string,
+  ): Promise<boolean> {
+    const userId = await redis.get(token);
+    
+    if (!userId) {
+      return false;
+    }
+
+    await User.update({ id: Number(userId) }, {confirmed: true})
+    await redis.del(token)
+
+    return true
+  }
+}
--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -3,6 +3,8 @@ import bcrypt from "bcryptjs";
 import { User } from "../../entity/User";
 import { RegisterInput } from "./register/RegisterInput";
 import { isAuth } from "../middleware/isAuth";
+import { sendEmail } from "../utils/sendEmail";
+import { createConfirmationUrl } from "../utils/createConfirmationUrl";
 
 @Resolver()
 export class RegisterResolver {
@@ -24,6 +26,8 @@ export class RegisterResolver {
       password: hashedPassword,
       email,
     }).save();
+
+    await sendEmail(email, createConfirmationUrl(user.id))
 
     return user;
   }
