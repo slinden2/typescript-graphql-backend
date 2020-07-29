d018dd4f322f01df58bc2ad1454d59386094770a
Samu <samu.linden@gmail.com>
2020-07-29 17:49:23 +0200

    Adds file upload functionality for profile pics"

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -7,6 +7,7 @@ import { LogoutResolver } from "../modules/user/Logout";
 import { MeResolver } from "../modules/user/Me";
 import { RegisterResolver } from "../modules/user/Register";
 import { CreateUserResolver } from "../modules/user/CreateUser";
+import { ProfilePictureResolver } from "../modules/user/ProfilePicture";
 
 export const createSchema = () =>
   buildSchema({
@@ -19,6 +20,7 @@ export const createSchema = () =>
       MeResolver,
       RegisterResolver,
       CreateUserResolver,
+      ProfilePictureResolver,
     ],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
